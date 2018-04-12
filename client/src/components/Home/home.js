80243d30977a17aa8982424ecec6a0cd4ffa5c56
Warnerotzy <adrianmartinez.sopra@gmail.com>
2018-04-12 13:28:53 +0200

    Finished Home

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomeContainer from '../../containers/home_container';
 
 
 const Home = () => {
     return (
         <div>
-            HOME
+            <HomeContainer />
         </div>
     );
 };
