cc4b924ab3a1fcbb0ba9020a0315969dbf78588f
Warnerotzy <adrianmartinez.sopra@gmail.com>
2018-04-12 11:53:46 +0200

    Added header to homepage

---
+++ client/src/components/Header/SideNav/sidenav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SideNav from "react-simple-sidenav";
+
+
+const Nav = (props) => {
+    return (
+        <SideNav
+            showNav={props.showNav}
+            onHideNav={props.onHideNav}
+            navStyle={{
+                background: '#ededed',
+                maxWidth: '220px'
+            }}
+        >
+            Items
+        </SideNav>
+    );
+};
+
+export default Nav;
